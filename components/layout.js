d7ef46e9da2dfed3fc72d59e6fc33f65677efc21
Martin Solev <martin.solev@tripx.se>
2021-01-10 13:35:56 +0100

    update meta description

--- components/layout.js
+++ components/layout.js
@@ -15,7 +15,7 @@ export default function Layout({ children, home }) {
         <link rel="icon" href="/favicon.ico" />
         <meta
           name="description"
-          content="Learn how to build a personal website using Next.js"
+          content="Martin Solev - Software and Cloud engineer building scalable apps"
         />
         <meta
           property="og:image"
